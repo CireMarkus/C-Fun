25debd42e8d54e006145096f7396ef256f927742
Eric Washington <mrkimjarrow95@gmail.com>
2020-03-14 15:36:48 -0500

    completed chapter 1 my first console application

---
+++ First Console app/First Console App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace First_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prompt user to enter a name.
+            Console.WriteLine("Enter your name, please");
+
+            //Now read the name entered.
+            string name = Console.ReadLine();
+
+            //Freet the user with the name that was entered.
+            Console.WriteLine("Hello, " + name);
+
+
+            //Wait for user to acknowlege the results. 
+            Console.WriteLine("Press Enter to terminate...");
+            Console.Read();
+        }
+    }
+}
